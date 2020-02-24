c15ce82ef46be0263c05c00a824e030d3ee67144
ahyaemon <iamahyaemon@gmail.com>
2020-02-24 14:23:18 +0900

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,54 @@
 <template lang="pug">
     #app
-        header
-            h1 虫食い算やろうぜ！！
+        MNav
         router-view
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import MNav from '@/layouts/MNav.vue'
+
+@Component({
+    components: {
+        MNav
+    }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+    * {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    body {
+        font-family: 游ゴシック, YuGothic, "ヒラギノ角ゴ Pro", "Hiragino Kaku Gothic Pro", メイリオ, Meiryo, sans-serif;
+        background-image: url('./assets/background.jpeg');
+    }
+
+    button, input, select, textarea {
+        font-family: inherit;
+        font-size: 100%;
+    }
+
+    ul {
+        padding: 0 0 0 10px;
+    }
+
+    li {
+        list-style-type: none;
+    }
+
+    table {
+        border-collapse: collapse;
+    }
+
+    a {
+        text-decoration: none;
+        color: rgb(100, 117, 99);
+        text-decoration: underline;
+    }
+</style>
---
+++ src/layouts/MNav.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+    header#luxbar.luxbar-static
+        input#luxbar-checkbox.luxbar-checkbox(type="checkbox" v-model="checked")
+        .luxbar-menu.luxbar-menu-left.luxbar-menu-dark
+            ul.luxbar-navigation(@click="closeMenu")
+                li.luxbar-header
+                    router-link.luxbar-brand(to="/") 虫食い算やろうぜ！！
+                    label#luxbar-hamburger.luxbar-hamburger.luxbar-hamburger-spin(for="luxbar-checkbox")
+                        span
+                li.luxbar-item(v-for="(link, ilink) in links" :key="ilink")
+                    router-link(:to="link.url") {{ link.title }}
+</template>
+
+<script lang="ts">
+import 'luxbar/build/luxbar.min.css'
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+interface Link {
+    url: string;
+    title: string;
+}
+
+@Component
+export default class MNav extends Vue {
+    private links: Link[] = [
+        { url: '/how', title: '解き方' },
+        { url: '/', title: '今週の問題' },
+        { url: '/search-question', title: '過去問たち' },
+        { url: '/column', title: 'コラム' },
+        { url: '/about', title: 'About' }
+    ]
+
+    private checked = false
+
+    private closeMenu () {
+        this.checked = false
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        border-bottom: solid 5px #BA6262;
+    }
+    .luxbar-brand {
+        padding: 18px 0px 18px 10px;
+    }
+    .luxbar-menu-dark {
+        background-color: rgb(37, 37, 37);
+    }
+</style>
